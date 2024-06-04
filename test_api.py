c24a5d92e54fb3c70ab6643d629473d707c600f1
juansouza09 <juan.souza.git@gmail.com>
2024-06-04 20:59:56 -0300

    pipeline updated

--- test_api.py
+++ test_api.py
@@ -1,18 +1,17 @@
 import pytest
-from app import app
+from app import create_app
 
 @pytest.fixture
 def client():
+    app = create_app()
     app.config['TESTING'] = True
     with app.test_client() as client:
-        yield client
+        with app.app_context():
+            yield client
 
 def test_get_resultados_nba(client):
     response = client.get('/v1/resultados_nba')
     assert response.status_code == 200
-    assert b'Los Angeles Lakers' in response.data
-    assert b'Golden State Warriors' in response.data
-    assert b'Brooklyn Nets' in response.data
-    assert b'Milwaukee Bucks' in response.data
-    assert b'Los Angeles Clippers' in response.data
-    assert b'Phoenix Suns' in response.data
+    data = response.get_json()
+    assert len(data) == 3
+    assert data[0]["time_casa"] == "Los Angeles Lakers"
